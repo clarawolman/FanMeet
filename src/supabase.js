3675f2a161e080d02108d9ac89d96d7bb9e20da3
LUCIANA GALIT KAMPEL <48860067@ort>
2026-05-18 08:32:55 -0300

    Agregando la base de datos

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'hvcnqqluazwucewvkdky'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2Y25xcWx1YXp3dWNld3ZrZGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNDg0MzEsImV4cCI6MjA5MzgyNDQzMX0.qZwhgavlDpsq9sZNCE75kkD7Nk8YE0rcMOVZmjoNbhU'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
