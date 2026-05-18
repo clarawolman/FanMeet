2a4f66348fe1c68fe3963c2089b73ddccc2660f4
LUCIANA GALIT KAMPEL <48860067@ort>
2026-05-18 09:57:40 -0300

    Conectando la base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -12,6 +12,35 @@ function App() {
       
     </>
   )
+}
+
+export default App*/
+import { useEffect } from 'react'
+import { supabase } from './supabase'
+
+function App() {
+
+   useEffect(() => {
+      probarConexion()
+   }, [])
+
+   async function probarConexion() {
+
+      const { data, error } = await supabase
+         .from('usuario')
+         .select('*')
+
+      if(error){
+         console.log(error)
+      }
+      else{
+         console.log(data)
+      }
+   }
+
+   return (
+      <h1>FanMeet</h1>
+   )
 }
 
 export default App
