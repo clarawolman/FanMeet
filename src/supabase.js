25334ec2e366f5bf6a5d0482d315930830af1ded
RAMIRO PITA <49261291@ort>
2026-06-29 08:18:01 -0300

    dotenv security por favor

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hvcnqqluazwucewvkdky.supabase.co'
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2Y25xcWx1YXp3dWNld3ZrZGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNDg0MzEsImV4cCI6MjA5MzgyNDQzMX0.qZwhgavlDpsq9sZNCE75kkD7Nk8YE0rcMOVZmjoNbhU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
